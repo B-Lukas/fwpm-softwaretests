8de24241216a9d4e853af210bdbf0cd35cabb9d3
P0sa <lukasbiernat7@gmail.com>
2018-11-23 16:38:35 +0100

    Updated Gitlab-ci file for Docker in Docker

--- src/main/java/PipelineTest/Greeter.java
+++ src/main/java/PipelineTest/Greeter.java
@@ -1,8 +1,9 @@
 package PipelineTest;
 
-public class Main {
+public class Greeter {
 
     public static void main(String[]args){
         System.out.print("Hello World");
+        System.out.print(Rechner.addition(2,2));
     }
 }
